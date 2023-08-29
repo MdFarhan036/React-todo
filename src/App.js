9db4a2affdcc9cde185d1b1f479c16b25405680b
MdFarhan036 <82930880+MdFarhan036@users.noreply.github.com>
2023-08-29 23:45:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
+import React, { useState } from 'react';
 import { NavBar } from './components/NavBar';
+import { Create } from './components/Createmodal';
 import './App.css';
+import { Card } from './components/Card';
 
 function App() {
+  const [addItem, setAddItem] = useState([]);
+  const addNote = (note) => {
+    setAddItem((data) => {
+      return [...data, note]
+    });
+  };
+  const onDelete = (id) => {
+    setAddItem((oldData) => {
+      oldData.filter((currdata, indx) => {
+        return indx !== id;
+      })
+    })
+  }
   return (
     <>
-    <NavBar />
+      <NavBar />
+      <Create passNote={addNote} />
+      {addItem.map((val, index) => {
+        return <Card
+          key={index}
+          id={index}
+          description={val.description}
+          status={val.status}
+          date={val.date}
+          deleteItem={onDelete}
+        />
+      })}
     </>
   );
 }
